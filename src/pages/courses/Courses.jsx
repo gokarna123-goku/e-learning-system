5b14f2afaa4e32db432b63cd8a3e036c38338e40
Gokarna Adhikari <agokarna444@gmail.com>
2024-05-29 11:30:50 +0545

    changes the images

--- src/pages/courses/Courses.jsx
+++ src/pages/courses/Courses.jsx
@@ -8,10 +8,10 @@ const Courses = () => {
             <h1 className='text-center text-3xl font-bold text-violet-600 mb-8'>Our Courses</h1>
             <div className="w-full grid md:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-5">
                 {/* Course Card */}
-                <CourseCard imgUrl={"https://cdn.pixabay.com/photo/2018/05/18/03/16/online-3410266_1280.jpg"} title={"React training from Beginner to Advance"} category={"Website"} price={"$120"} rating={"4.7"} totalStudents={"100"} />
-                <CourseCard imgUrl={"https://cdn.pixabay.com/photo/2018/05/18/03/16/online-3410266_1280.jpg"} title={"Figma from Beginner to Advance Course"} category={"UX/UI Design"} price={"$100"} rating={"4.8"} totalStudents={"100"} />
-                <CourseCard imgUrl={"https://cdn.pixabay.com/photo/2018/05/18/03/16/online-3410266_1280.jpg"} title={"HTML, CSS, & JavaScript from Scratch"} category={"Website"} price={"$89"} rating={"5"} totalStudents={"100"} />
-                <CourseCard imgUrl={"https://cdn.pixabay.com/photo/2018/05/18/03/16/online-3410266_1280.jpg"} title={"Become a Full-Stack App Developer"} category={"UX/UI Design"} price={"$160"} rating={"4.9"} totalStudents={"100"} />
+                <CourseCard imgUrl={"https://cdn.pixabay.com/photo/2016/12/28/09/36/web-1935737_1280.png"} title={"React training from Beginner to Advance"} category={"Website"} price={"$120"} rating={"4.7"} totalStudents={"100"} />
+                <CourseCard imgUrl={"https://cdn.pixabay.com/photo/2016/04/21/13/27/computer-1343393_1280.jpg"} title={"Figma from Beginner to Advance Course"} category={"UX/UI Design"} price={"$100"} rating={"4.8"} totalStudents={"100"} />
+                <CourseCard imgUrl={"https://cdn.pixabay.com/photo/2022/06/16/11/14/web-development-7265717_1280.png"} title={"HTML, CSS, & JavaScript from Scratch"} category={"Website"} price={"$89"} rating={"5"} totalStudents={"100"} />
+                <CourseCard imgUrl={"https://cdn.pixabay.com/photo/2022/07/08/05/38/freelance-work-7308505_1280.png"} title={"Become a Full-Stack App Developer"} category={"UX/UI Design"} price={"$160"} rating={"4.9"} totalStudents={"100"} />
             </div>
             <div className="w-full mt-10 flex items-center justify-center">
                 <button className="bg-violet-600 hover:bg-transparent text-neutral-50 hover:text-violet-600 border border-violet-600 px-5 py-2 rounded-md ease-in-out duration-300 flex items-center gap-x-2">
